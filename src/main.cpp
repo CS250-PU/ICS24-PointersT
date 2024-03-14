5693985792729244d0cf01826f3df37cb74fd68f
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-03-14 20:36:43 +0000

    Added basic pointer examples

--- src/main.cpp
+++ src/main.cpp
@@ -14,5 +14,31 @@ using namespace std;
 
 int main () {
 
+  // *** pointer to character example
+  char *pCh, ch;
+
+  pCh = &ch; // addressOf
+  *pCh = 'A'; // dereference
+
+  cout << "Size of pCh is " << sizeof (pCh) << endl;
+  cout << "Size of ch is " << sizeof (ch) << endl;
+  cout << ch << " " << *pCh << endl; // dereference}
+
+  // *** pointer to integer example
+  int x, *pX;
+
+  x = 8;   // set x to a value of 8
+
+  pX = &x; // set the pointer variable to point
+           // to the address of x
+
+  cout << "x is: " << x << endl;
+  cout << "Size of x is: " << sizeof (x) << endl;
+  cout << "Address of x is: " << pX << endl;
+  cout << "Address of x is: " << &x << endl;
+
+
   return EXIT_SUCCESS;
+
 }
+
